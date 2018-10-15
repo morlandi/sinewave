d60b9a132b9e87f2911fc6f83e60e799919d1631
Mario Orlandi <morlandi@brainstorm.it>
2018-10-15 10:12:09 +0200

    Add sleep_time optional parameter to publisher

--- sinewave_listener.py
+++ sinewave_listener.py
@@ -1,10 +1,16 @@
 import redis
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 
-pubsub = r.pubsub()
-pubsub.subscribe('sinewave')
+def main():
+    r = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 
-for item in pubsub.listen():
-    if item['type'] == 'message':
-        print(item['data'])
+    pubsub = r.pubsub()
+    pubsub.subscribe('sinewave')
+
+    for item in pubsub.listen():
+        if item['type'] == 'message':
+            print(item['data'])
+
+
+if __name__ == "__main__":
+    main()
--- sinewave_publisher.py
+++ sinewave_publisher.py
@@ -1,11 +1,34 @@
 import math, time
 import redis
+import argparse
+import logging
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-for n in range(0, 100000):
-    row = (62 * 'X')[0:32 + int(30*math.sin(n/8.0))]
-    r.publish('sinewave', row)
-    #print(row)
-    print('\x1b[1;36;40m' + row + '\x1b[0m')
-    time.sleep(0.01)
+def publish(sleep_time):
+
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    dt = float(sleep_time) / 1000.0
+
+    for n in range(0, 100000):
+        row = (62 * 'X')[0:32 + int(30 * math.sin(n / 8))]
+        r.publish('sinewave', row)
+        #print(row)
+        print('\x1b[1;36;40m' + row + '\x1b[0m')
+        #time.sleep(0.01)
+        time.sleep(dt)
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description='Publish a sinewave on redis "sinewave" channel'
+    )
+    parser.add_argument('--sleep_time', '-s', type=int, default=10, help="expressed in [ms]; default = 10")
+    args = parser.parse_args()
+
+    while True:
+        publish(args.sleep_time)
+
+
+if __name__ == "__main__":
+    main()
